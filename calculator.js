b7d07c2eeb0b23ae5b804739676b250c57a800e2
Liliam Oliveira <89716594+Lihsolive@users.noreply.github.com>
2022-09-26 23:24:14 -0300

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,7 +1,6 @@
 function calculadora() {
     const operacao = Number(prompt('Escolha uma operação:\n 1 - Soma (+)\n 2 - Subtração (-)\n 3 - Multiplicação (*)\n 4 - Divisão Real (/)\n 5 - Divisão Inteira (%)\n 6 - Potenciação (**)'));
-
-
+    
     if (!operacao || operacao >= 7) {
         alert('Erro! Operação inválida!')
         calculadora()
@@ -65,21 +64,6 @@ function calculadora() {
                 }
             }
         }
-
-        // if (operacao == 1) {
-        //     soma();
-        // } else if (operacao == 2) {
-        //     subtracao();
-        // } else if (operacao == 3) {
-        //     multiplicacao();
-        // } else if (operacao == 4) {
-        //     divisaoReal();
-        // } else if (operacao == 5) {
-        //     divisaoInteira();
-        // } else if (operacao == 6) {
-        //     potenciacao();
-        // }
-
         switch (operacao) {
             case 1: 
                 soma()
